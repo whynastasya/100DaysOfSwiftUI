1cddc1ed6547eaecc98445c229fe9975f8349085
nastasya <grigorchukanastasiia@gmail.com>
2024-07-15 18:23:49 +0300

    initial

---
+++ 100DaysOfSwiftUI/100DaysOfSwiftUI/_00DaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysOfSwiftUIApp.swift
+//  100DaysOfSwiftUI
+//
+//  Created by nastasya on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct _00DaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
